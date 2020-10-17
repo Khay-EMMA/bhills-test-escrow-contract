b61a4b7ac99d05f7c87ecddbedc4eeb9a473e164
Njoku Emmanuel <Njoku Emmanuel>
2020-10-17 10:24:02 +0100

    write test for confirm purchase

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,12 +6,25 @@ contract("EscrowService", (accounts) => {
     const seller = accounts[0]
     
     beforeEach(async () => {
+    
         contractInstance = await EscrowService.new();
+    
     });
 
 
     it("Should abort the purchase", async () => {
+        
         const result = await contractInstance.abort({from : seller})
-        console.log(result)
+        
+       assert.equal(result.receipt.status, true, "tx reciept status is true")
+        
+    })
+
+    it("Should confirm purchase", async () => {
+        
+        const result = await contractInstance.confirmPurchase({from : accounts[1]})
+        
+      return assert.equal(result.receipt.status, true, "tx reciept status is true")
+    
     })
 })
